5b0971c0efcb1c53545d41fe6cec90836602ccb8
Asanov Sanzhar <asnvsn@mail.ru>
2022-11-29 15:51:24 +0600

    Initial Commit

---
+++ FinanceStocks/FinanceStocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceStocksApp.swift
+//  FinanceStocks
+//
+//  Created by Санжар Асанов on 29/11/22.
+//
+
+import SwiftUI
+
+@main
+struct FinanceStocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
